5db9e4e139af46b8db9200d942b7a5201827f01f
Nakamura shinichirou <naka5313@gmail.com>
2011-09-10 12:22:48 +0900

    remove nwword_validator.rb

--- app/models/nwword_validator.rb
+++
@@ -1,9 +0,0 @@
-class NgwordValidator < ActiveModel::EachValidator
-  ##カスタムバリデーションメソッド
-  def validate_each(record, attribute, value)
-    ngword = ['hoge', 'ugo']
-    if ngword.include?(value)
-      record.errors[attribute] << 'include ng word'
-    end
-  end
-end
